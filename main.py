d69c122b5f3d2acb23438d366c59c2c39c5a0b7b
Laurent Jouron <jouronlaurent@hotmail.com>
2022-03-13 10:45:45 +0100

    commit du 13/03/2022

--- main.py
+++ main.py
@@ -1,18 +1,43 @@
 from tinydb import TinyDB, Query, where
-from tinydb.storages import MemoryStorage
+
 import constants
 
 nombre_de_personne = constants.personne
 
 
 class PersonneModel:
+    """ In file"""
+    name_liste = input("Saisissez le nom de la liste : ") + ".json"
+    db = TinyDB(name_liste, indent=4)
+    
     def __init__(self, first_name, last_name):
         self.first_name = first_name
         self.last_name = last_name
     
     def __str__(self):
-        return f"Bonjour à tous je m'appelle " \
-               f"{self.first_name} {self.last_name}.\n"
+        return f"hello, my name is {self.first_name} {self.last_name}."
+    
+    @staticmethod
+    def add_personne():
+        if len(PersonneModel.db) >= nombre_de_personne:
+            print("Registration is complete.")
+        else:
+            personne = PersonneController.create_personne()
+            PersonneModel.db.insert({"first_name": personne.first_name,
+                                     "last_name": personne.last_name})
+            
+    @staticmethod
+    def display_personne():
+        for personne in PersonneModel.db:
+            print(personne)
+
+    @staticmethod
+    def delete_personne():
+        for personne in PersonneModel.db:
+            print(personne)
+        first_name = input("Enter the first name of the person you want to "
+                           "delete: ").capitalize()
+        PersonneModel.db.remove(where("first_name") == first_name)
 
 
 class PersonneController:
@@ -26,37 +51,27 @@ class PersonneController:
 class PersonneView:
     @staticmethod
     def get_information_personne():
-        first_name: str = input("Quel est votre prénom ? ")
-        last_name: str = input("Précisez votre nom de famille : ")
+        first_name: str = input("What's your first name ? ")
+        last_name: str = input("what's is your last name : ")
         return first_name.capitalize(), last_name.capitalize()
 
 
 if __name__ == '__main__':
     """Main"""
-""" In memory"""
-# db = TinyDB(storage=MemoryStorage, indent=4)
 
-""" In file"""
-db = TinyDB('personne.json', indent=4)
+"""Sentence when opening the game."""
+welcome = "Press - 1 to add , 2 to display, 3 for delete"
+print(f"\n{welcome.center(90, ' ')}")
 
 while True:
-    action = input("1 ajouter , 2 afficher, 3 supprimer : ")
+    action = input("You want Add - Display - Delete ? ")
     if action == "1":
-        if len(db) >= nombre_de_personne:
-            print("Les inscriptions sont terminées.")
-        else:
-            personne = PersonneController.create_personne()
-            db.insert({"first_name": personne.first_name, "last_name":
-                personne.last_name})
+        PersonneModel.add_personne()
     elif action == "2":
-        for personne in db:
-            print(personne)
+        PersonneModel.display_personne()
     elif action == "3":
-        for personne in db:
-            print(personne)
-        first_name = input("Saisissez le prénom de la personne à supprimer : ")
-        db.remove(where("first_name") == first_name)
+        PersonneModel.delete_personne()
     else:
         action = False
         if not action:
-            print("Erreur de saisie")
+            print("Input error")
