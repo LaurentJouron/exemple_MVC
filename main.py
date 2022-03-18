1c4f66d7bde1c0dd23e33f70e933ddeba8736bd6
Laurent Jouron <jouronlaurent@hotmail.com>
2022-03-18 09:19:33 +0100

    Functional Personne Add, Display and Delete 18/03/2022

--- main.py
+++ main.py
@@ -7,88 +7,93 @@ nombre_de_personne = constants.personne
 
 class PersonneModel:
     db = TinyDB("personne.json", indent=4)
-    
+
     def __init__(self, first_name, last_name):
         self.first_name = first_name
         self.last_name = last_name
-    
-    def __repr__(self):
+
+    def __str__(self):
         return f"hello, my name is {self.first_name} {self.last_name}."
-    
+
+    def __repr__(self):
+        return f"{self.first_name} {self.last_name}"
+
     def save(self):
         PersonneModel.db.insert({"first_name": self.first_name,
-                   "last_name": self.last_name})
-        
+                                 "last_name": self.last_name})
+
     @staticmethod
     def get_all():
         return PersonneModel.db
-    
-    # @staticmethod
-    # def display_personne():
-    #     for personne in PersonneModel.db:
-    #         print(personne)
-    
-    # @staticmethod
-    # def delete_personne():
-    #     for personne in PersonneModel.db:
-    #         PersonneModel.db.remove(where("first_name") == first_name)
-    
-    # @staticmethod
-    # def delete_data():
-    #     db.truncate()
+
+    def delete(self):
+        return PersonneModel.db.remove(where("first_name") == self.first_name)
 
 
 class PersonneController:
     @staticmethod
-    def create_personne():
+    def create():
         first_name, last_name = PersonneView.get_information_personne()
         personne = PersonneModel(first_name, last_name)
         personne.save()
         return personne
-    
+
     @staticmethod
     def get_all():
         personnes_model = []
         personnes = PersonneModel.get_all()
         for personne in personnes:
-            personne_model = PersonneModel(personne["first_name"], personne["last_name"])
+            personne_model = PersonneModel(personne["first_name"],
+                                           personne["last_name"])
             personnes_model.append(personne_model)
         return PersonneView.display_all(personnes_model)
-        
+
+    @staticmethod
+    def delete():
+        first_name, last_name = PersonneView.delete()
+        personne = PersonneModel(first_name, last_name)
+        personne.delete()
+        return personne
+
 
 class PersonneView:
     @staticmethod
     def get_information_personne():
-        first_name: str = input("What's your first name ? ").capitalize()
-        last_name: str = input("what's is your last name : ").capitalize()
+        first_name: str = input("What's the first-name ? ").capitalize()
+        last_name: str = input("what's is the last-name : ").capitalize()
         return first_name, last_name
-    
+
     @staticmethod
     def display_all(personnes):
         for personne in personnes:
             print(personne)
 
+    @staticmethod
+    def delete():
+        first_name: str = input("Which person first-name you want cancel ? ").capitalize()
+        last_name: str = input("Which person last-name you want cancel ? ").capitalize()
+        return first_name, last_name
+
+
 if __name__ == '__main__':
     """Main"""
 
-"""Sentence when opening the game."""
-welcome = "1 to add , 2 to display, 3 for delete player, 4 for delete all"
-print(f"\n{welcome.center(90, ' ')}")
 
 while True:
-    action = input("You want Add - Display - Delete player - delete all ? ")
+    """Sentence when opening the game."""
+    welcome = " Select: 1-> Add  2-> display  3-> delete "
+    print(f"\n{welcome.center(90, '-')}\n")
+    action = input("What you want to do ? ")
     if action == "1":
-        PersonneController.create_personne()
+        PersonneController.create()
     elif action == "2":
         PersonneController.get_all()
-    # elif action == "3":
-    #     PersonneModel.delete_personne()
-    # elif action == "4":
-    #     PersonneModel.delete_data()
-    # else:
-    #     action = False
-    #     if not action:
-    #         print("Input error")
+    elif action == "3":
+        PersonneController.delete()
+    else:
+        action = False
+        if not action:
+            print("Input error")
 
 """
 faire une application de todo liste
